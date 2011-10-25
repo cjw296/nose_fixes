63fe06a004c73d41521bf64abe9adf6d4c0fc30d
Chris Withers <chris@simplistix.co.uk>
2011-10-25 17:15:42 +0100

    implement and test options

--- nose_fixes/plugin.py
+++ nose_fixes/plugin.py
@@ -13,12 +13,19 @@ class BetterLoader(TestLoader):
     
 class Plugin(NosePlugin):
 
-    name = 'nose_collect'
+    name = 'nose_fixes'
 
     enabled = True
     
     def options(self, parser, env):
-        pass
+        parser.add_option("--test-suite-func", action="store",
+                          dest="test_suite_func",
+                          default='test_suite',
+                          help="A function in modules that will return a TestSuite. "
+                               "Defaults to 'test_suite'.")
     
+    def configure(self, options, config):
+        self.test_suite_func = options.test_suite_func
+        
     def prepareTestLoader(self, loader):
         return BetterLoader()
---
+++ nose_fixes/tests/test_plugin.py
@@ -0,0 +1,29 @@
+from nose.config import Config
+from optparse import OptionParser
+from unittest import TestCase
+
+class Tests(TestCase):
+
+    def setUp(self):
+        from nose_fixes.plugin import Plugin
+        self.plugin = Plugin()
+        
+    def test_enabled(self):
+        self.assertTrue(self.plugin.enabled)
+
+    def test_suite_defined(self):
+        self.assertEqual(self.plugin.name, 'nose_fixes')
+
+    def _parse_args(self, *args):
+        parser = OptionParser()
+        self.plugin.addOptions(parser)
+        options, args = parser.parse_args(list(args))
+        self.plugin.configure(options, Config())
+        
+    def test_suite_name_default(self):
+        self._parse_args()
+        self.assertEqual(self.plugin.test_suite_func, 'test_suite')
+    
+    def test_suite_name_override(self):
+        self._parse_args('--test-suite-func=foo')
+        self.assertEqual(self.plugin.test_suite_func, 'foo')
