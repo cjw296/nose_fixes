f07ca9a6a41d40be519e32962351927b47bfb914
Chris Withers <chris@simplistix.co.uk>
2011-11-17 18:49:05 +0000

    better functional tests

--- nose_fixes/tests/test_docstrings.py
+++ nose_fixes/tests/test_docstrings.py
@@ -2,19 +2,16 @@ from imp import new_module
 from nose.case import Test
 from nose.core import TestProgram
 from nose.loader import TestLoader
-from unittest import TestCase
+from nose.plugins import PluginTester 
+from nose_fixes.plugin import Plugin
+from unittest import TestCase, makeSuite
 
 class Tests(TestCase):
 
     def setUp(self):
-        from nose_fixes.plugin import Plugin
         self.plugin = Plugin()
         
     def test_prepareTestResult_sets_descriptions(self):
-        "This test has a docstring"
-        # The above is a functional test you can use
-        # by running nose with the plugin and making
-        # sure the docstring isn't shown by default
         thing = object()
         self.plugin.show_docstrings = thing
         class FakeResult(object):
@@ -23,3 +20,34 @@ class Tests(TestCase):
         result = FakeResult()
         self.plugin.prepareTestResult(result)
         self.assertEqual(result.descriptions, thing)
+
+class FunctionalMixin(PluginTester):
+
+    plugins = [Plugin()]
+    activate = ''
+    args = ['-v']
+
+    def makeSuite(self):
+        class TC(TestCase):
+            def test_name0(self):
+                'confusing output!'
+                pass
+            def test_name1(self):
+                'confusing output!'
+                self.fail()
+        return makeSuite(TC)
+    
+class TestHideDocStrings(FunctionalMixin, TestCase):
+
+    def test(self):
+        self.assertFalse('confusing output!' in self.output)
+        self.assertTrue('name0' in self.output)
+        self.assertTrue('name1' in self.output)
+
+class TestShowDocStrings(FunctionalMixin, TestCase):
+
+    activate = '--show-docstrings'
+
+    def test(self):
+        self.assertTrue('confusing output!' in self.output)
+        self.assertFalse('name0' in self.output)
